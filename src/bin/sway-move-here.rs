7d5ade350c37f8b4e9b6c11396bd16c6704ffcc5
Stas Rudakou <stas@garage22.net>
2019-08-04 15:22:42 +0300

    Add CLI options using docopt

--- src/bin/sway-move-here.rs
+++ src/bin/sway-move-here.rs
@@ -1,6 +1,30 @@
+use docopt::Docopt;
+use serde::Deserialize;
+
 use sway_move_here::{SwayOutputs, SwayWorkspaces, swaymsg};
 
+const USAGE: &'static str = "
+Move all the active workspaces to the focused output in Sway window manager.
+
+Usage:
+  sway-move-here [-v]
+  sway-move-here (-h | --help)
+
+Options:
+  -v            Verbose output.
+  -h --help     Show this screen.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    flag_v: bool,
+}
+
 fn main() {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+
     let sway_outputs: SwayOutputs = swaymsg(vec!["-t", "get_outputs"]);
     let focused_output = sway_outputs.get_focused_output().unwrap();
     let sway_workspaces : SwayWorkspaces = swaymsg(vec!["-t", "get_workspaces"]);
