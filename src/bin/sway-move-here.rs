e3bfd4618eb8a7ac461fa59a103d9d016c0dbade
Stas Rudakou <stas@garage22.net>
2019-08-04 15:38:23 +0300

    Deduplicate logic for calling swaymsg command

--- src/bin/sway-move-here.rs
+++ src/bin/sway-move-here.rs
@@ -1,7 +1,7 @@
 use docopt::Docopt;
 use serde::Deserialize;
 
-use sway_move_here::{SwayOutputs, SwayWorkspaces, swaymsg};
+use sway_move_here::{SwayOutputs, SwayWorkspaces, swaymsg_and_deserialize};
 
 const USAGE: &'static str = "
 Move all the active workspaces to the focused output in Sway window manager.
@@ -25,8 +25,8 @@ fn main() {
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
 
-    let sway_outputs: SwayOutputs = swaymsg(vec!["-t", "get_outputs"]);
+    let sway_outputs: SwayOutputs = swaymsg_and_deserialize(vec!["-t", "get_outputs"]);
     let focused_output = sway_outputs.get_focused_output().unwrap();
-    let sway_workspaces : SwayWorkspaces = swaymsg(vec!["-t", "get_workspaces"]);
+    let sway_workspaces : SwayWorkspaces = swaymsg_and_deserialize(vec!["-t", "get_workspaces"]);
     sway_workspaces.move_to_output(&focused_output);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::process::{Command, Output as CommandOutput};
 use std::vec::Vec;
 
 use serde::Deserialize;
@@ -47,8 +47,12 @@ impl SwayWorkspaces {
     }
 }
 
-pub fn swaymsg<T>(args: Vec<&str>) -> T where T: DeserializeOwned {
-    let output = Command::new("swaymsg").args(args).output().expect("failed to execute process");
+pub fn swaymsg(args: Vec<&str>) -> CommandOutput {
+    Command::new("swaymsg").args(args).output().expect("failed to execute process")
+}
+
+pub fn swaymsg_and_deserialize<T>(args: Vec<&str>) -> T where T: DeserializeOwned {
+    let output = swaymsg(args);
     let stdout = String::from_utf8(output.stdout).unwrap();
 
     let sway_data: T = serde_json::from_str(&stdout).unwrap();
