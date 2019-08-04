896a7b669368b605239ad4fae6768f0f86913975
Stas Rudakou <stas@garage22.net>
2019-08-04 16:19:48 +0300

    cargo fmt for the first time

--- src/bin/sway-move-here.rs
+++ src/bin/sway-move-here.rs
@@ -1,8 +1,10 @@
 use docopt::Docopt;
 use serde::Deserialize;
-use simplelog::{Config as LogConfig, LevelFilter as LogLevelFilter, TermLogger, TerminalMode as LogTerminalMode};
+use simplelog::{
+    Config as LogConfig, LevelFilter as LogLevelFilter, TermLogger, TerminalMode as LogTerminalMode,
+};
 
-use sway_move_here::{SwayOutputs, SwayWorkspaces, swaymsg_and_deserialize};
+use sway_move_here::{swaymsg_and_deserialize, SwayOutputs, SwayWorkspaces};
 
 const USAGE: &'static str = "
 Move all the active workspaces to the focused output in Sway window manager.
@@ -26,11 +28,20 @@ fn main() {
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
 
-    let log_level_filter = if args.flag_v { LogLevelFilter::Info } else { LogLevelFilter::Warn };
-    TermLogger::init(log_level_filter, LogConfig::default(), LogTerminalMode::Mixed).unwrap();
+    let log_level_filter = if args.flag_v {
+        LogLevelFilter::Info
+    } else {
+        LogLevelFilter::Warn
+    };
+    TermLogger::init(
+        log_level_filter,
+        LogConfig::default(),
+        LogTerminalMode::Mixed,
+    )
+    .unwrap();
 
     let sway_outputs: SwayOutputs = swaymsg_and_deserialize(vec!["-t", "get_outputs"]);
     let focused_output = sway_outputs.get_focused_output().unwrap();
-    let sway_workspaces : SwayWorkspaces = swaymsg_and_deserialize(vec!["-t", "get_workspaces"]);
+    let sway_workspaces: SwayWorkspaces = swaymsg_and_deserialize(vec!["-t", "get_workspaces"]);
     sway_workspaces.move_to_output(&focused_output);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,14 @@ use std::process::{Command, Output as CommandOutput};
 use std::vec::Vec;
 
 use log::info;
-use serde::Deserialize;
 use serde::de::DeserializeOwned;
+use serde::Deserialize;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct SwayOutput {
     name: String,
     focused: bool,
-    current_workspace: String
+    current_workspace: String,
 }
 
 #[derive(Deserialize, Debug)]
@@ -41,8 +41,16 @@ impl SwayWorkspaces {
             if workspace.output != output.name {
                 let workspace_filter = format!("[workspace=\"{}\"]", workspace.name);
                 Command::new("swaymsg")
-                    .args(&[&workspace_filter, "move", "workspace", "to", "output", &output.name])
-                    .output().unwrap();
+                    .args(&[
+                        &workspace_filter,
+                        "move",
+                        "workspace",
+                        "to",
+                        "output",
+                        &output.name,
+                    ])
+                    .output()
+                    .unwrap();
             }
         }
     }
@@ -50,12 +58,18 @@ impl SwayWorkspaces {
 
 pub fn swaymsg(args: Vec<&str>) -> CommandOutput {
     info!("Calling swaymsg with args {:?}", &args);
-    let output = Command::new("swaymsg").args(args).output().expect("failed to execute process");
+    let output = Command::new("swaymsg")
+        .args(args)
+        .output()
+        .expect("failed to execute process");
     info!("Exit code: {}", output.status.code().unwrap_or(-1));
     output
 }
 
-pub fn swaymsg_and_deserialize<T>(args: Vec<&str>) -> T where T: DeserializeOwned {
+pub fn swaymsg_and_deserialize<T>(args: Vec<&str>) -> T
+where
+    T: DeserializeOwned,
+{
     let output = swaymsg(args);
     let stdout = String::from_utf8(output.stdout).unwrap();
     info!("Standard output: {}", stdout);
@@ -63,7 +77,6 @@ pub fn swaymsg_and_deserialize<T>(args: Vec<&str>) -> T where T: DeserializeOwne
     let sway_data: T = serde_json::from_str(&stdout).unwrap();
     sway_data
 }
-
 
 #[cfg(test)]
 mod tests {
